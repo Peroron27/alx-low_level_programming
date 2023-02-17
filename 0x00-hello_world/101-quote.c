d73c0cb78c8bfbca6a333cb7be49755c5ae364df
Peroron27 <aniepete@gmail.com>
2023-02-16 21:40:10 -0800

    operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 {#include <stdio.h>
 
 	int main(void)
-	( "and that piece of art is useful" - Dora korpar, 2015-10-19\n", 61);
-
-			return (1);
+	{write(2,"and that piece of art is useful" - Dora korpar, 2015-10-19\n", 61);
+	return (1);
 	}}
