cd46e9768b321958cd1dcb81b8cad5ef14248330
Peroron27 <aniepete@gmail.com>
2023-02-19 00:45:43 -0800

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * 
- * Return: Always 0 (success/correct)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 	char i;
 
-	for (i = 'a'; i<= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
+
