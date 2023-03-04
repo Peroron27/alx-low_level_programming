4d306ac3f2442bb977dc1e6e105c135ba435356d
Peroron27 <aniepete@gmail.com>
2023-03-04 14:39:43 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,27 @@ void times_table(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10; y++)
-		{
-			k = x * y;
-			if (y == 0)
-				_putchar(k + '0');
-			if (y != 0 && k < 10)
-			{
-				_putchar(',');
-	        		_putchar(' ');
-				_putchar(' ');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar('\n');
+	for (y = 0; y < 10; y++)
+	{
+	k = x * y;
+	if (y == 0)
+	{
+	_putchar(k + '0');
+	if (y != 0 && k < 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	}
+	else if (k >= 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((k / 10) + '0');
+	}
+	_putchar((k % 10) + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
