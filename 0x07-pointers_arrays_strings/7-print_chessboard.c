7368563cea7755b08041d22c56e09eff77ffbbbf
Peroron27 <aniepete@gmail.com>
2023-03-06 20:32:17 -0800

    set string

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
-*print_chessboard - print the chessboard, 2d array
-*@a: 2d array of chars
-*Return: void
-*/
-void print_chessboard(char (*a)[8])
-{
-/*Declaring variables*/
-int i, j;
+ * print_chessboard - entry point
+ * @a: array
+ * Return Always 0 (success)
+ */
 
-i = 0;
-while (i < 8) /*number repetitions*/
+void print_chessboard(char (*a)[8]);
 {
-j = 0;
-while (j < 8)
-{
-_putchar (a[i][j]);
-j++;
-}
-_putchar ('\n'); /*print new line*/
-i++; /*add +1*/
-}
 
+	int i;
+	int j;
+
+	{
+	for (i = o; i = 8; i++);
+	
+		for (j = 0; j = 8; j++)
+			_putchar(a[i][j]);
+	_putchar('\n');
+	}
 }
