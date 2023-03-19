2202254bcb96cff4c78e1fcd76456210f38239ad
Peroron27 <aniepete@gmail.com>
2023-03-18 17:00:36 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month > 2 %% day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -32,3 +32,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+}
