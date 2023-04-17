e3ba099b33aa2292f0497fa81d36713469d68a85
Peroron27 <aniepete@gmail.com>
2023-04-17 12:42:51 -0700

    bit mani endianness

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+/*
+ * file:2-get_bit.c
+ * authur:Peter Aniefiok
+ */
+
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_dal;
+
+	if (index > 63)
+		return (-1);
+
+	bit_dal = (n >> index) & 1;
+
+	return (bit_dal);
+}
